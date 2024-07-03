cfe42c9a7e7306cdd3aaedc893c698ac5b083f31
chengr25 <384372602@qq.com>
2024-07-03 20:21:41 +0800

    fix:添加用户名已存在的员工 异常处理

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
  */
@@ -22,6 +24,17 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public Result duplicateNameHandler(SQLIntegrityConstraintViolationException ex){
+        String message = ex.getMessage();
+        if(message.contains("Duplicate entry")){
+            String key = message.split(" ")[2].replaceAll("\'","");
+            log.error( "账号为 " + key + " 的员工已经存在");
+            return Result.error("账号为 " + key + " 的员工已经存在");
+        }
+        return  Result.error("未知错误");
     }
 
 }
